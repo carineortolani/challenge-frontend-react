156b850d359b060482cf2eae199d34d72685cd97
carineortolani <carine.ortolani@liveonsolutions.com>
2021-07-26 17:14:09 -0300

    feat(home): adding home page and header to the page

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+import logo from '../../assets/logo.svg'
+import iconUsers from '../../assets/users.svg'
+
+const PageHeader = () => (
+  <header className="header">
+    <div className="container">
+      <img src={logo} alt="Logo" className="header--logo"/>
+      <button className="header--button-team">
+        Your team
+        <img class="button--icon" src={iconUsers} alt="Icon Users" />
+      </button>
+    </div>
+  </header>
+)
+
+export default PageHeader
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../../components/Header'
+
+const Home = () => (
+  <React.StrictMode>
+    <PageHeader />
+  </React.StrictMode>
+)
+
+export default Home
