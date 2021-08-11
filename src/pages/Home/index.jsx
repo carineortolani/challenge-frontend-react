4dac5695dbbec8b19f4f79050b74631fb79d7902
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-11 16:09:40 -0300

    refactor(Home): renamed section for banner

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../layout/Header'
-import Section from '../../layout/Banner'
+import Banner from '../../layout/Banner'
 import Search from '../../components/Search'
 import Titles from '../../components/Titles'
 import ListCards from '../../components/ListCards'
@@ -9,9 +9,9 @@ import Footer from '../../layout/Footer'
 const Home = () => (
   <React.StrictMode>
     <Header />
-    <Section>
+    <Banner>
       <Search />
-    </Section>
+    </Banner>
     <Titles title="Characters" subtitle="# results"/>
     <ListCards />
     <Footer />
