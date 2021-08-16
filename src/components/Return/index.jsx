f1dd80ffef11faf0a89976226bc5f3029f1ceabf
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-16 10:54:30 -0300

    fix(Return): adjust back button to return to correct page

--- src/components/Return/index.jsx
+++ src/components/Return/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import css from './Return.module.sass'
 import iconReturn from '../../assets/icons/return.svg'
 
-const Return = () => (
-  <Link to="/">
-    <button className={css.btnReturn}>
+const Return = () => {
+  const history = useHistory()
+  const back = () => history.goBack()
+
+  return (
+    <button className={css.btnReturn} onClick={back}>
       Return to previous page
       <img src={iconReturn} alt="Return" className={css.icon}/>
     </button>
-  </Link>
-)
+  )
+}
+
 
 export default Return
