9a6693ef6673f35ad16807c82ac81c66eb1f977d
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-16 16:09:34 -0300

    feat(card): adding hero favorite function

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './Card.module.sass'
 import { Link } from 'react-router-dom'
 import spider from '../../assets/images/spider.png'
 
-const Card = () => (
-  <Link to="/profile" className={css.card}>
-    <img src={spider} alt="" className={css.imgHero} />
-    <button className={css.btnFavorite}></button>
-    <h4 className={css.hero}> {/* {hero} */} Spider Man </h4>
-    <p className={css.description}> {/* {description} */} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel convallis velit. </p>
-  </Link>
-)
+const Card = () => {
+  const [favoriteHero, setFavoriteHero] = useState(JSON.parse(localStorage.getItem('@app-Marvel/YourTeam')) || [])
+
+  const favorite = e => {
+    e.preventDefault();
+    setFavoriteHero([...favoriteHero, { favorite: true }])
+  }
+
+  return (
+    <Link to="/profile" className={css.card}>
+      <img
+        src={spider}
+        alt="Hero"
+        className={css.imgHero}
+      />
+      <button
+        className={css.btnFavorite}
+        title="Favorite Hero"
+        onClick={favorite}
+      />
+      <h4 className={css.hero}> {/* {hero} */} Spider Man </h4>
+      <p className={css.description}> {/* {description} */} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel convallis velit. </p>
+    </Link>
+  )
+}
 
 export default Card
