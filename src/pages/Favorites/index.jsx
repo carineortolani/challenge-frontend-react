2bcdbca31f0b279d4dc3e0a10a0d17d7cffdbf60
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-18 12:13:09 -0300

    refactor: adjust styles

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../layout/Header'
 import Banner from '../../layout/Banner'
+import Return from '../../components/Return'
 import Favorites from '../../components/Favorites'
 import Footer from '../../layout/Footer'
 
@@ -8,6 +9,7 @@ const TeamFavorite = () => (
   <React.StrictMode>
     <Header />
     <Banner>
+      <Return />
       <Favorites />
     </Banner>
     <Footer />
