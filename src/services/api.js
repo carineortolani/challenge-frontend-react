94d313cc9b0ef7c50d187ea39d9abe0892e5b35e
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-18 16:23:11 -0300

    feat(api): adding api to receive characters data

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 import { MD5 } from 'crypto-js'
 
 const ts = Date.now()
-const baseUrl = process.env.REACT_APP_BASE_URL
+const baseURL = process.env.REACT_APP_BASE_URL
 const publicKey = process.env.REACT_APP_PUBLIC_KEY
 const privateKey = process.env.REACT_APP_PRIVATE_KEY
 const hash = MD5(ts + privateKey + publicKey).toString()
 
 export const api = axios.create({
-  baseUrl,
+  baseURL,
   params: { apikey: publicKey, ts, hash }
 })
 
