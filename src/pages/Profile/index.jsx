59f53a3e3796a5eb35c123b49e44878a2e7a1b61
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-23 16:09:40 -0300

    refactor(page/profile): passing properties to components

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,38 +1,60 @@
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 import { getProfile } from '../../services/profile'
+import { getComics } from '../../services/comics'
 
 import Banner from '../../layout/Banner'
 import Character from '../../components/Character'
-// import Titles from '../../components/Titles'
+import Titles from '../../components/Titles'
+import Comic from '../../components/Comic'
 
 import css from './Profile.module.sass'
 
 const Profile = () => {
-const [profile, setProfile] = useState({})
+  const [profile, setProfile] = useState({})
+  const [comics, setComics] = useState({})
+  let { id } = useParams()
 
-useEffect(() => {
-  getProfile()
-    .then(response => setProfile(response))
-    .catch(error => console.error(error))
-}, [])
+  useEffect(() => {
+    getProfile(id)
+      .then(response => setProfile(response))
+      .catch(error => console.error(error))
 
-return (
-  <>
-    <Banner>
-      <Character
-        name={profile.name}
-        description={profile.description}
-        thumbnail={profile.thumbnail}
-      />
-    </Banner>
-    <div className={css.container}>
-      {/* <Titles title="Characters" subtitle={`${comics.total || `#`} results`} /> */}
-      <div className={css.comicsList}>
+    getComics(id)
+      .then(response => setComics(response))
+      .catch(error => console.error(error))
+  }, [id])
+  console.log(comics)
 
+  return (
+    <>
+      <Banner>
+        {profile.results?.map((result, key) => (
+          <Character
+            key={key}
+            name={result.name}
+            description={result.description}
+            thumbnail={result.thumbnail}
+          />
+        ))}
+      </Banner>
+      <div className={css.container}>
+        <Titles title="Comics" subtitle={`${comics.total || `#`} results`} />
+        <div className={css.comicsList}>
+          {comics.results?.map(result => (
+            <Comic
+              key={result.id}
+              images={result.images}
+              title={result.title}
+              pages={result.pages}
+              description={result.description}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-)}
+    </>
+  )
+}
 
 export default Profile
