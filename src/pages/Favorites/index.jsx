b069f63db551cacd81ea94f0f3e95ead65bbfdab
carineortolani <carine.ortolani@liveonsolutions.com>
2021-08-25 16:02:52 -0300

    refactor(pages/favorites): adjust to receive team favorite

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import css from './Favorites.module.sass'
 import Banner from '../../layout/Banner'
-import Return from '../../components/Return'
-import Favorites from '../../components/Favorites'
 
-const TeamFavorite = () => (
-  <>
-    <Banner>
-      <Return />
-    </Banner>
-    <Favorites />
-  </>
-)
+// import Card from '../../components/Card'
+import Return from '../../components/Return'
+
+const TeamFavorite = () => {
+
+
+    return (
+    <>
+      <Banner>
+        <div className={css.container}>
+          <Return />
+          <h1 className={css.title}>Here is your own strike team choice</h1>
+        </div>
+      </Banner>
+
+      {/* <div className={css.cardsList}>
+        {teamFavorite?.map((result, key) => (
+          <Card
+            key={key}
+            id={result.id}
+            name={result.name}
+            description={result.description}
+            thumbnail={result.thumbnail}
+          />
+        ))}
+      </div> */}
+    </>
+  )
+}
 
 export default TeamFavorite
